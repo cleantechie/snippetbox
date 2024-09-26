211690ab1405ff3164c722d04b5c069dd21d0a64
harshasv <surahvardhan@gmail.com>
2024-09-27 00:20:49 +0530

    logging via dependency injection

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,14 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(response http.ResponseWriter, request *http.Request) {
+func (applogger *logger) home(response http.ResponseWriter, request *http.Request) {
 	if request.URL.Path != "/" {
 		http.NotFound(response, request)
 		return
@@ -19,32 +17,33 @@ func home(response http.ResponseWriter, request *http.Request) {
 		"../../ui/html/pages/home.html",
 	)
 	if ERROR != nil {
-		log.Print(ERROR.Error())
+		applogger.errLogger.Print(ERROR.Error())
 		http.Error(response, "Internal Server Error", http.StatusInternalServerError)
 	}
 	ERROR = TEMPLATE.ExecuteTemplate(response, "base", nil)
 	if ERROR != nil {
-		log.Print(ERROR.Error())
+		applogger.errLogger.Print(ERROR.Error())
 		http.Error(response, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func snippetCreate(response http.ResponseWriter, request *http.Request) {
+func (applogger *logger) snippetCreate(response http.ResponseWriter, request *http.Request) {
 	if request.Method != "POST" {
 		response.Header().Set("Allow", "POST")
 		http.Error(response, "This Method isnt allowed", http.StatusMethodNotAllowed)
 
 	}
+	applogger.infoLogger.Print("Creating an new Snippet")
 	response.Header().Set("Content-Type", "application/json")
 	response.Write([]byte(`{"name" : "test"}`))
 }
 
-func snippetView(response http.ResponseWriter, request *http.Request) {
+func (applogger *logger) snippetView(response http.ResponseWriter, request *http.Request) {
 	snippetId, ERROR := strconv.Atoi(request.URL.Query().Get("snippetId"))
 	if ERROR != nil || snippetId < 1 {
 		http.NotFound(response, request)
 		return
 	}
-	fmt.Fprintf(response, "Displaying a specfic snippet with Id %d....", snippetId)
+	applogger.infoLogger.Printf("Displaying a specfic snippet with Id %d....", snippetId)
 	response.Write([]byte("Display a new Snippet"))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,34 +7,48 @@ import (
 	"os"
 )
 
+type logger struct {
+	infoLogger *log.Logger
+	errLogger *log.Logger
+}
+
+
 func main() {
 	host := flag.String("host", ":5000", "Http Port")
 	flag.Parse()
+
 	mux := http.NewServeMux()
+
+	// creating an combined object of both these loggers so then can be injected
+	appLogger :=  &logger {
 	// Info log instance
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC)
+	infoLogger : log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC),
 	// Err log instance
-	errLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.LUTC|log.Lshortfile)
+	errLogger : log.New(os.Stdout, "ERROR\t", log.Ldate|log.LUTC|log.Lshortfile),	
+	} 
 
 	// Get the files from the directory
 	fileServer := http.FileServer(http.Dir("../../ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	// Home Page Request
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/", appLogger.home)
+
 	// POST REQ to create a snippet
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/snippet/create", appLogger.snippetCreate)
+
 	// GET REQ to view a snippet with snippetId
-	mux.HandleFunc("/snippet/view", snippetView)
-	infoLog.Printf("Starting server on port %s", *host)
+	mux.HandleFunc("/snippet/view", appLogger.snippetView)
+
+	appLogger.infoLogger.Printf("Starting server on port %s", *host)
 
 	// new server struct so even http error will use the err log instance
 	server := &http.Server {
 		Addr: *host,
 		Handler: mux,
-		ErrorLog: errLog,
+		ErrorLog: appLogger.errLogger,
 	}
 
 	serverStartErr := server.ListenAndServe()
-	errLog.Fatal(serverStartErr)
+	appLogger.errLogger.Fatal(serverStartErr)
 }
