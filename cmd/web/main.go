b4fb780128c85dd83528e80479c48384016f89b7
cleantechie <surahvardhan@gmail.com>
2024-09-27 00:50:57 +0530

    commit

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,9 +9,8 @@ import (
 
 type logger struct {
 	infoLogger *log.Logger
-	errLogger *log.Logger
+	errLogger  *log.Logger
 }
-
 
 func main() {
 	host := flag.String("host", ":5000", "Http Port")
@@ -20,12 +19,13 @@ func main() {
 	mux := http.NewServeMux()
 
 	// creating an combined object of both these loggers so then can be injected
-	appLogger :=  &logger {
-	// Info log instance
-	infoLogger : log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC),
-	// Err log instance
-	errLogger : log.New(os.Stdout, "ERROR\t", log.Ldate|log.LUTC|log.Lshortfile),	
-	} 
+	appLogger := &logger{
+		// Info log instance
+		infoLogger: log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC),
+
+		// Error log instance
+		errLogger: log.New(os.Stdout, "ERROR\t", log.Ldate|log.LUTC|log.Lshortfile),
+	}
 
 	// Get the files from the directory
 	fileServer := http.FileServer(http.Dir("../../ui/static"))
@@ -43,9 +43,9 @@ func main() {
 	appLogger.infoLogger.Printf("Starting server on port %s", *host)
 
 	// new server struct so even http error will use the err log instance
-	server := &http.Server {
-		Addr: *host,
-		Handler: mux,
+	server := &http.Server{
+		Addr:     *host,
+		Handler:  mux,
 		ErrorLog: appLogger.errLogger,
 	}
 
